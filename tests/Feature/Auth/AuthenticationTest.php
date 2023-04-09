85829043801076185dd9701d31419a7fe379d224
poymanov <n.poymanov@gmail.com>
2023-04-09 10:09:41 +0300

    Добавлена система авторизации

---
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('login screen can be rendered', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
+
+it('users can authenticate using the login screen', function () {
+    $user = User::factory()->create();
+
+    $response = $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(RouteServiceProvider::HOME);
+});
+
+it('users can not authenticate with invalid password', function () {
+    $user = User::factory()->create();
+
+    $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+    ]);
+
+    $this->assertGuest();
+});
