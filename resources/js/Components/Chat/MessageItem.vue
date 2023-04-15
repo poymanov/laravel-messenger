8c7616aeae9e66de3f0b6824612fabd2c03b7bcb
poymanov <n.poymanov@gmail.com>
2023-04-15 15:42:57 +0300

    Добавлен отступ между сообщениями

--- resources/js/Components/Chat/MessageItem.vue
+++ resources/js/Components/Chat/MessageItem.vue
@@ -16,7 +16,7 @@ const isCurrentUserSender = currentAuthUserId === props.message.sender_user_id;
 
 <template>
     <p
-        class="px-6 py-3 rounded-full  max-w-xs lg:max-w-md text-gray-200"
+        class="px-6 py-3 rounded-full max-w-xs lg:max-w-md text-gray-200 mb-2"
         :class="{'bg-gray-800': !isCurrentUserSender, 'bg-blue-700': isCurrentUserSender}"
     >{{ message.text }} </p>
 </template>
